96a3bdc0ad77dd061993d6e17b30188164b0d726
vlad <vladas272@gmail.com>
2023-10-15 10:07:39 +0300

    xxx

--- index.js
+++ index.js
@@ -1,51 +1,58 @@
-$(document).ready(function() {
-    const slides = $('.slide');
-    let currentSlide = 0;
+    $(document).ready(function() {
+        const slides = $('.slide');
+        let currentSlide = 0;
 
-    function showSlide() {
-        $(slides).hide(); // Hide all slides
-        $(slides[currentSlide]).show(); // Show the current slide
+        function showSlide() {
+            $(slides).hide(); // Hide all slides
+            $(slides[currentSlide]).show(); // Show the current slide
 
-        const numItems = $('.slide:visible .item').length;
-        $(".slide:visible span").css("opacity", "0"); // Reset opacity
-        setTimeout(() => { // This timeout ensures the opacity reset is applied before starting the transition
-            for (let i = 1; i <= numItems; i++) {
-                $(".slide:visible .item:nth-child(" + i + ")").css({
-                    "opacity": "1",
-                    "transition": "opacity 0.7s ease",
-                    "transition-delay": i / 10 + "s"
-                });
-            }
-        }, 0);
+            
+            
+
+            const numItems = $('.slide:visible .item').length;
+            $(".slide:visible span").css("opacity", "0"); // Reset opacity
+            setTimeout(() => { // This timeout ensures the opacity reset is applied before starting the transition
+                for (let i = 1; i <= numItems; i++) {
+                    $(".slide:visible .item:nth-child(" + i + ")").css({
+                        "opacity": "1",
+                        "transition": "opacity 0.7s ease",
+                        "transition-delay": i / 10 + "s"
+                    });
+                }
+            }, 0);
+        }
+
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % slides.length;
+        showSlide();
     }
 
-function nextSlide() {
-    currentSlide = (currentSlide + 1) % slides.length;
-    showSlide();
-}
+        // Convert each p.item_holder's text to individual span items
+        $(".slide p.items_holder").each(function() {
+            const textContent = $(this).text();
+            const spanizer = $.trim(textContent).split("");
+            $(this).html('<span class="item">' + spanizer.join('</span><span class="item">') + '</span>');
+        });
 
-    // Convert each p.item_holder's text to individual span items
-    $(".slide p.items_holder").each(function() {
-        const textContent = $(this).text();
-        const spanizer = $.trim(textContent).split("");
-        $(this).html('<span class="item">' + spanizer.join('</span><span class="item">') + '</span>');
-    });
+        // Detect swipe to the left
+        let touchStartX = 0;
+        let touchEndX = 0;
 
-    // Detect swipe to the left
-    let touchStartX = 0;
-    let touchEndX = 0;
+        $("#mainHolder").on('touchstart', function(event) {
+            touchStartX = event.changedTouches[0].screenX;
+        });
 
-    $("#mainHolder").on('touchstart', function(event) {
-        touchStartX = event.changedTouches[0].screenX;
-    });
+        $("#mainHolder").on('touchend', function(event) {
+            touchEndX = event.changedTouches[0].screenX;
+            if (touchEndX < touchStartX) {
+                nextSlide();
+            }
+        });
 
-    $("#mainHolder").on('touchend', function(event) {
-        touchEndX = event.changedTouches[0].screenX;
-        if (touchEndX < touchStartX) {
-            nextSlide();
-        }
-    });
+        // Start by showing the first slide
+        showSlide();
 
-    // Start by showing the first slide
-    showSlide();
+        $("#swipeIcon").on('click', function() {
+    nextSlide();
 });
+    });
